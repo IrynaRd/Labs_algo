6c56bd77a2515157b6f311c8d7990c989ef53c3a
IrynaRd <irudnytska07@gmail.com>
2024-09-11 20:20:59 +0300

    adding my lab1

---
+++ lab1/task1.py
@@ -0,0 +1,26 @@
+
+car = "Citroen"
+print(car)
+
+
+
+is_student = True
+is_teacher = False
+
+
+
+#Вивід результатів логічних операцій
+
+print('is_student And is_teacher:', is_student and is_teacher)
+print('is_student Or is_teacher:', is_student or is_teacher)
+print('Not is_student:', not is_student)
+print('Not is_teacher', not is_teacher)
+
+
+
+is_red = False
+is_blue = True
+
+#Вивід результату логічного "або"
+
+print('is_red Or is_blue:', is_red or is_blue)
