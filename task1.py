76ad49c30cd9da1255ab3fe4d9a53dd39241af73
IrynaRd <irudnytska07@gmail.com>
2024-09-23 16:00:30 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-
+#Визначення функцій
 def fun_1(x):
     return math.atan(1 / x)
 
@@ -11,6 +11,7 @@ def fun_2(x):
 def fun_3(x):
     return 1 / (1 + math.log(x))
 
+#Табуляція
 def tab_func(a, b, h):
     value_x = np.arange(a, b + h, h)
     res = []
@@ -33,5 +34,6 @@ b = 3.5
 
 tabul = tab_func(a, b, h)
 
+#Вивід результатів
 for x, y in tabul:
     print(f'x = {x:.2f}, f(x) = {y:.2f}')
