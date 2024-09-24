a47b64f9e7236ce62af9e25781c765badfd4cb51
IrynaRd <irudnytska07@gmail.com>
2024-09-24 17:13:25 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 #Визначення функції
 def cum_sum(x, d):
     n = 1
@@ -15,15 +13,15 @@ def cum_sum(x, d):
 
 #Табуляція 
 def tab_cumsum(a, b, h, d):
-    value_x = np.arange(a, b + h, h)
     res = []
+    x = round(a, 2)
 
-    for x in value_x:
-        y = cum_sum(x, d)
-        res.append((x, y))
+    while x <= b:
+        fun_y = cum_sum(x, d)
+        res.append((x, fun_y))
+        x = round(x + h, 3)
 
     return res
-
 
 a = -0.5
 b = 0.0
@@ -33,6 +31,6 @@ d = 0.001
 res = tab_cumsum(a, b, h, d)
 
 #Вивід результатів
-for x, y in res:
-    print(f"x = {x: .3f}, f(x) = {y: .3f}")
+for x,  fun_y in res:
+    print(f"x = {x: .3f}, f(x) = {fun_y: .3f}")
 
